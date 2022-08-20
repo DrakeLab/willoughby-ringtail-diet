da0cb4ae9d40def7dfa78785af78db9e713c4237
arw36 <arwill19@gmail.com>
2022-08-20 11:13:58 -0400

    edit map script for facetting

---
+++ scripts/04-map-charts.R
@@ -0,0 +1,30 @@
+# load libraries 
+library(tidyverse) # data wrangling
+
+# load data
+studies <- read.csv('data/studies.csv')
+# filter to only included studies
+studies$X <- NULL 
+studies <- studies %>% 
+  dplyr::filter(Inclusion_Status == "include")
+# select out columns of interest
+er_data <- select(studies, source_id, Latitude, Longitude, ecoregion_lvl1, er1_name, er1_color, Decade)
+
+# add in ecoregions with no data 
+er_data[nrow(er_data) + 1,] <- c("","","","","Marine West Coast Forest", "","")
+er_data[nrow(er_data) + 1,] <- c("","","","","Tropical Wet Forests", "","")
+er_data$er1_name <- as.factor(er_data$er1_name )
+er_data <- er_data %>% dplyr::filter(Decade != "")
+
+# plot histogram timeline by ecoregion
+er_plot <- ggplot(er_data, aes(x = Decade)) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~er1_name) + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) + 
+  ylab('Studies') +
+  xlab('') # + 
+#  theme(plot.title= element_text(hjust=.5), 
+#        panel.background = element_rect(fill=unique(er_data$er1_color)))
+
+er_plot
