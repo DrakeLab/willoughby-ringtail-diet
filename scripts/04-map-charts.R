03c6e0be4f1c388079045c1da0bc320d64193cbf
arw36 <arwill19@gmail.com>
2022-08-22 11:15:38 -0400

    add annotations to plot

--- scripts/04-map-charts.R
+++ scripts/04-map-charts.R
@@ -16,16 +16,17 @@ er_data[nrow(er_data) + 1,] <- c(NA,NA,NA,NA,"Tropical Wet Forests","#7c7c7c",NA
 er_data$er1_name <- as.factor(er_data$er1_name )
 
 # plot histogram timeline by ecoregion
-er_plot <- ggplot(er_data, aes(x = as.numeric(Decade), group = er1_name)) +
+er_plot <- ggplot(er_data, aes(x = as.numeric(Decade), group = er1_name)) + 
+  # plot rectangle first because elements will be stacked 
   geom_rect(
     aes(xmin=-Inf, xmax=Inf,
-                 ymin=0, ymax=4),    # totally defined by trial-and-error
-                 fill=er_data$er1_color) + 
-  geom_histogram() +
-  facet_wrap(~ er1_name, labeller = labeller(er1_name = label_wrap_gen(width = 25)), scales = 'free') + 
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) + 
-  theme_classic() +
-  scale_x_continuous(limits=c(1920,2025)) + scale_y_continuous(limits=c(0,4)) + 
+                 ymin=0, ymax=4),    # size of plot area
+                 fill=er_data$er1_color) + # outside aes so colors are read as their identity and not assigned
+  geom_histogram() + # count of studies by decade
+  facet_wrap(~ er1_name, labeller = labeller(er1_name = label_wrap_gen(width = 25)), scales = 'free') + # each ecoregion gets own plot
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) + # rotate x axis tick labels
+  theme_classic() + # minimal graph elements
+  scale_x_continuous(limits=c(1920,2025)) + scale_y_continuous(limits=c(0,4)) + # make sure x axis on all facet rows
   ylab('Studies') +
   xlab('')
  
